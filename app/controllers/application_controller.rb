06dc2f492111ce271b7624de00055ea3ffac817d
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2017-01-11 21:45:28 +0600

    Implemented the basic login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
   def hello
     render html: "Hello Mahmud, welcome back to Rails5 :D "
